68fd7b904430b49325af31b173d950bb14e0612b
nikitaivantsov <i79307046480@gmail.com>
2018-10-16 16:36:03 +0300

    mongodb

--- index.js
+++ index.js
@@ -1,45 +1,59 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID; 
 
 var app = express(); // сервер
+var db;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
-
-var people = [
-	{
-		id : 1,
-		name : 'Niki'
-	},
-	{
-		id : 2,
-		name : 'Ilya'
-	}
-];
-
 
 app.get('/', function(req , res) {
 	res.send('Hello, World');
 });
 
 app.get('/people', function(req, res) {
-	res.send(people);
+	//res.send(people);
+	db.collection('people1').find().toArray(function(err, docs) {
+		if (err) {
+			console.log(err);
+			return res.sendStatus(500);
+		}
+		res.send(docs);
+	});
 });
 
 app.post('/people', function(req, res) {
-	var p  = {
-		id : Date.now(),
+	var people  = {
 		name : req.body.name
 	};
+	db.collection('people1').insert(people, function(err, result) {
+		if (err) {
+			console.log(err);
+			return res.sendStatus(500);
+		}
+		res.send(people);
+	});
+	/*
 	people.push(p);
 	console.log(req.body);
 	res.send(people);
+	*/
 });
 
 app.get('/people/:id', function(req, res){
-	var man, find = false;
+	db.collection('people1').findOne({ _id : ObjectID(req.params.id)}, function (err, docs) {
+		if (err) {
+			console.log(err);
+			return res.sendStatus(500);
+		}
+		else {
+			res.send('Hello, ' + docs.name + '\n ID : ' + docs._id);
+		}
+	});
+	/*var man, find = false;
 	people.forEach( function(item) {
 		if(item.id == req.params.id) {
 			man = item;
@@ -52,9 +66,46 @@ app.get('/people/:id', function(req, res){
 	} 
 	else {
 		res.send('Пользователя с таким ID не существует');
-	}
+	}*/
+});
+	
+app.put('/people/:id', function(req, res) {
+	db.collection('people1').updateOne(
+		{ _id  : ObjectID(req.params.id)},
+		{ name : req.body.name },
+		function(err, result) {
+			if(err) {
+				console.log(err);
+				return res.sendStatus(500);
+			}
+			res.sendStatus(200); 
+		}
+	);
 });
 
-app.listen('3000', function() {
-	console.log('Port : 3000');
+app.delete('/people/:id', function(req, res) {
+	db.collection('people1').deleteOne(
+		{ _id : ObjectID(req.params.id) },
+		function(err, result) {
+			if(err) {
+				console.log(err);
+				return res.sendStatus(500);
+			}
+			res.sendStatus(200);
+		}	
+	);
 });
+
+MongoClient.connect('mongodb://localhost:27017/myapp', function(err, database) {
+	if (err) {
+		return console.log(err);
+	}
+	else {
+		db = database;
+		app.listen('3000', function() {
+		console.log('Port : 3000');
+		});
+	}
+	
+});
+
